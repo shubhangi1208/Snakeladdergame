fd205737186945115e9f86d0da471299fe97361f
shubhangi1208 <ashubhangi1996@gmail.com>
2021-12-26 19:02:33 +0530

    Added:first player and start position zero

--- src/snakeandladdergame.java
+++ src/snakeandladdergame.java
@@ -3,6 +3,9 @@ public class snakeandladdergame {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake ladder game");
+		final	int NUMBER_OF_PLAYER=1;
+		final	int START_POSITION=0;
+		
 
 	}
 
