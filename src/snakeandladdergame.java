ad5af0b54865d97e7bb22e21f536ffd21aa6d3c0
shubhangi1208 <ashubhangi1996@gmail.com>
2022-01-02 18:38:47 +0530

    added snake and ladder game

--- src/snakeandladdergame.java
+++ src/snakeandladdergame.java
@@ -1,56 +1,69 @@
 import java.util.*;
+
 public class snakeandladdergame {
+    public static void main(String[] args) {
+        numberOfPlayers();
+    }
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to snake ladder game");
-		final	int NUMBER_OF_PLAYER=1;
-		final	int NO_PLAY=1;
-	    final	int LADDER=2;
-	    final	int SNAKE=3;
-	    int START_POSITION=0;
-	    
-	    int position=START_POSITION;
+//    function to initialize the number of players
+    static void numberOfPlayers(){
+//        setting the player's start position to 0
+        int playerOnePosition = 0;
+        playGame(playerOnePosition);
+    }
+
+//    function to play the game
+    static void playGame(int playerOnePosition){
         Random random = new Random();
-        
-		 //ROLL DIE FOR PLAYER
-		 int rollDie= random.nextInt(6);
-		 
-		 //CHECK FOR OPTION
-		 int checkOption=random.nextInt(3);
-		 
-		 switch(checkOption) {
-		 
-		 case 0:{
-			 System.out.println("No Play: ");
-             System.out.println("Die face value: " + rollDie);
-			 
-		 }
-		 case 1:{
-			 System.out.println("Ladder: ");
-             System.out.println("Die face value: " + rollDie);
-             if(START_POSITION < 100){
-                 int currentPosition = START_POSITION + rollDie;
-                 if(currentPosition <= 100){
-                	 START_POSITION = currentPosition;
-                	 
-                 }
-             }
-		  }
-		 case 3 :{
-             System.out.println("Snake: ");
-             System.out.println("Die face value: " + rollDie);
-             
-             if(START_POSITION > 0){
-                 int currentPosition = START_POSITION - rollDie;
-                 if(currentPosition >= 0){
-                	 START_POSITION = currentPosition;
-                 }
-             else
-                 System.out.println("You can't go down any further.");
-             }
-         }
-		 
-		}
-	}
+        int dieRollCount = 0;
+        System.out.println("Player One's current position: " + playerOnePosition);
+        while (playerOnePosition != 100){
+            int dieRoll = random.nextInt(1, 7);
+            dieRollCount++;
+            playerOnePosition = playAvailableOptions(playerOnePosition, dieRoll);
+            System.out.println("Player One's current position: " + playerOnePosition);
+            System.out.println();
+        }
+        System.out.println("Congratulations! You completed the game successfully.");
+        System.out.println("The die was rolled " + dieRollCount + " number of times to win the game.");
+    }
 
-}		 
+//        function to play available options
+    static int playAvailableOptions(int playerOnePosition, int dieRoll){
+        Random random = new Random();
+        int availableOptions = random.nextInt(1, 4);
+        switch(availableOptions){
+            case 1 -> {
+                System.out.println("No Play: ");
+                System.out.println("Die face value: " + dieRoll);
+            }
+            case 2 -> {
+                System.out.println("Ladder: ");
+                System.out.println("Die face value: " + dieRoll);
+                if(playerOnePosition < 100){
+                    int currentPosition = playerOnePosition + dieRoll;
+                    if(currentPosition <= 100){
+                        playerOnePosition = currentPosition;
+                    }
+                    else{
+                        System.out.println("You need to get the exact die face value to reach near or exactly 100 and not more than that ");
+                    }
+                }
+            }
+            case 3 -> {
+                System.out.println("Snake: ");
+                System.out.println("Die face value: " + dieRoll);
+                if(playerOnePosition > 0){
+                    int currentPosition = playerOnePosition - dieRoll;
+                    if(currentPosition >= 0){
+                        playerOnePosition = currentPosition;
+                    }
+                else{
+                    System.out.println("You can't go down any further.");
+                }
+                }
+            }
+        }
+        return playerOnePosition;
+    }
+}
